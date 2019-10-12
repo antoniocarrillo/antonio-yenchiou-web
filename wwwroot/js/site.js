28862be0eb39fcafe614920f7809439613045b92
Antonio Banders <antonio.carrillo.ce@gmail.com>
2019-10-12 20:36:25 +0200

    Move to dotnet 3.0 and made it publishable

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var image = document.getElementsByClassName('parallax-image');
+new simpleParallax(image, {scale:1.5);
